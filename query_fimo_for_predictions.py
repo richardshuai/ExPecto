0126944d11b4f5173ec9a3f8808899dc8f93b964
Richard Shuai <richardshuai@berkeley.edu>
2022-07-14 22:22:58 -0400

    switch to vcf for fimo queries, assume symmetric padding

--- query_fimo_for_predictions.py
+++ query_fimo_for_predictions.py
@@ -9,14 +9,13 @@ import pyfasta
 
 def main():
     parser = argparse.ArgumentParser(description='Query FIMO for each SNP in SED df')
-    parser.add_argument("--sed_file")
+    parser.add_argument("--vcf_file")
     parser.add_argument("--motif_file")
     parser.add_argument("--chunk_size", action="store", dest="chunk_size", type=int, default=None,
                         help="Size of chunks for batching predictions")
     parser.add_argument("--chunk_i", action="store", dest="chunk_i", type=int, default=None,
                         help="Chunk index for current run, starting from 0")
-    parser.add_argument("--upstream_bp", default=30)
-    parser.add_argument("--downstream_bp", default=30)
+    parser.add_argument("--bp_pad", default=30)
     parser.add_argument("--pval_match_threshold", default=1e-4)
     parser.add_argument("--hg19_fasta", default="resources/hg19.fa")
     parser.add_argument('-o', dest="out_dir", type=str, default='temp_query_fimo_for_predictions', help='Output directory')
@@ -26,17 +25,18 @@ def main():
     os.makedirs(args.out_dir, exist_ok=True)
 
     # Read sed df
-    sed_df = pd.read_csv(args.sed_file, sep="\t").iloc[:, 2:]
+    vcf_df = pd.read_csv(args.vcf_file, sep="\t", comment="#",
+                         names=["CHROM", "POS", "ID", "REF", "ALT" , "QUAL", "FILTER", "INFO"])
 
     if args.chunk_i is not None:
-        sed_df = sed_df.iloc[args.chunk_i * args.chunk_size:(args.chunk_i + 1) * args.chunk_size]
+        vcf_df = vcf_df.iloc[args.chunk_i * args.chunk_size:(args.chunk_i + 1) * args.chunk_size]
 
-    sed_df["seq"] = sed_df.apply(lambda x: read_seq(x[0], x[1], x["strand"], x[3], x[4],
-                                                    args.hg19_fasta, args.upstream_bp, args.downstream_bp), axis=1)
+    vcf_df["seq"] = vcf_df.apply(lambda x: read_seq(x["CHROM"], x["POS"], x["REF"], x["ALT"],
+                                                    args.hg19_fasta, args.bp_pad), axis=1)
 
     fimo_in_fasta = f"{args.out_dir}/fimo_in.fasta"
     with open(fimo_in_fasta, "w") as f:
-        for _, row in sed_df.iterrows():
+        for _, row in vcf_df.iterrows():
             fasta_id = row[2]
             print(f'>{fasta_id}', file=f)
             print(row["seq"], file=f)
@@ -60,22 +60,14 @@ def main():
     fimo_df.to_csv(f"{args.out_dir}/fimo_filtered.tsv", sep="\t", header=True)
 
 
-def read_seq(chrom, pos, strand, ref_snp, alt_snp, hg19_fasta, upstream_bp, downstream_bp):
+def read_seq(chrom, pos, ref_snp, alt_snp, hg19_fasta, bp_pad):
     genome = pyfasta.Fasta(hg19_fasta)
-    if strand == "+":
-        start = pos - upstream_bp  # 1-based
-        stop = pos + downstream_bp  # 1-based, inclusive
-    elif strand == "-":
-        start = pos - downstream_bp  # 1-based
-        stop = pos + upstream_bp  # 1-based, inclusive
+    start = pos - bp_pad  # 1-based
+    stop = pos + bp_pad  # 1-based, inclusive
 
     seq = genome.sequence({'chr': chrom, 'start': start, 'stop': stop}).upper()
 
-    if strand == "+":
-        assert (seq[upstream_bp] == ref_snp) or (seq[upstream_bp] == alt_snp), "fasta does not match VCF"
-    elif strand == "-":
-        assert (seq[downstream_bp] == ref_snp) or (seq[downstream_bp] == alt_snp), "fasta does not match VCF"
-
+    assert (seq[bp_pad:bp_pad + len(ref_snp)] == ref_snp) or (seq[bp_pad:bp_pad + len(alt_snp)] == alt_snp), "fasta does not match VCF"
     return seq
 
 
